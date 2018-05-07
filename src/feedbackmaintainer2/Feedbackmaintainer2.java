64541e79b3a1f2d2147de926515902bb25cf270f
chinmay367 <chinmaychoudhary29@gmail.com>
2018-05-08 01:00:37 +0530

    feedback

---
+++ src/feedbackmaintainer2/Feedbackmaintainer2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feedbackmaintainer2;
+
+/**
+ *
+ * @author CS
+ */
+public class Feedbackmaintainer2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      mainframe obj=new mainframe();
+      obj.setVisible(true);
+    }
+    
+}
